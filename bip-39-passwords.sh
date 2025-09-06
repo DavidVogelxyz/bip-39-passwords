5c3fadefb9af4a4f59f6fd086e65cd0588f5e04a
David Vogel <david@davidvogel.xyz>
2025-09-06 16:02:51 -0600

    REFACTOR: major reworking of the script
    multiple changes were made to the script, including:
    - SHA256 checksum verification of the wordlist
    - cleaner functions and code in general

--- bip-39-passwords.sh
+++ bip-39-passwords.sh
@@ -1,108 +1,111 @@
 #!/bin/sh
 
+wordlist="wordlist.txt"
+checksum="2f5eed53a4727b4bf8880d8f3f199efc90e58503646d9ff8eff3a2ed3b24dbda"
+tmpdir="/tmp/bip-39-passwords"
+tmpfile="${tmpdir}/${wordlist}"
+url_seedwords="https://raw.githubusercontent.com/bitcoin/bips/refs/heads/master/bip-0039/english.txt"
+
 #####################################################################
 
-# this functions grabs the wordlist
-# prefers the `wordlist.txt` local to the repo
-# if that doesn't exist, check the `/tmp` directory
-# if that doesn't exist, grab the wordlist from GitHub
-get_wordlist() {
-    if [[ -z wordlist.txt ]]; then
-        if [[ -z /tmp/bip-39-passwords/wordlist.txt ]]; then
-            mkdir -p /tmp/bip-39-passwords
-            curl https://raw.githubusercontent.com/bitcoin/bips/refs/heads/master/bip-0039/english.txt > /tmp/bip-39-passwords/wordlist.txt
-        fi
+verify_checksum() {
+    local hash="$(sha256sum $1)"
+    hash="${hash%% *}"
 
-        wordlist="/tmp/bip-39-passwords/wordlist.txt"
-    else
-        wordlist="wordlist.txt"
-    fi
+    [[ "$checksum" == "$hash" ]]
 }
 
-# this function will get some randomness
-# `$1` is the range (ex. 1-100)
+# prints argument to STDERR and exits
+error() {
+    echo "$1" >&2 \
+        && exit 1
+}
+
+# Prefer the repo's `wordlist.txt`
+# If no local `wordlist.txt`, use one in `/tmp/bip-39-passwords`
+# Download from official Bitcoin repo if necessary
+get_wordlist() {
+    if [[ -f "$wordlist" ]] && verify_checksum "$wordlist"; then
+        return 0
+    fi
+
+    echo "[WARN]: \`${wordlist}\` not found, or invalid. Now checking \`${tmpfile}\`."
+
+    if [[ ! -f ${tmpfile} ]]; then
+        echo "[WARN]: \`${tmpfile}\` not found. Attempting to download now..."
+        mkdir -p "$tmpdir"
+        curl -s "$url_seedwords" > "$tmpfile" \
+            || error "[ERROR]: Unable to download a copy of the wordlist. Exiting now."
+    fi
+
+    wordlist="$tmpfile"
+
+    if ! verify_checksum "$wordlist"; then
+        error "[ERROR]: No valid wordlist. Exiting now."
+    fi
+
+    return 0
+}
+
+# Takes in a range (ex. "1-100"), outputs random number within range
 get_rng() {
     shuf -n 1 -i $1
 }
 
 #####################################################################
 
-ask_number_words() {
-    read -r -p "How many words you want? " number_words
+ask_wordcount() {
+    read -r -p "How many words do you want in the password? " wordcount
 
-    while ! echo "$number_words" | grep -q "^[0-9][0-9]*$"; do
-        read -r -p "A NUMBER, PLEASE." number_words
+    while ! echo "$wordcount" | grep -q "^[0-9][0-9]*$"; do
+        read -r -p "A NUMBER, PLEASE." wordcount
     done
 
-    echo "$number_words"
+    echo "$wordcount"
 }
 
-# this function gets a random number between 1 and 2048
-# then, the function looks up the word at that value
-get_word() {
-    number=$(get_rng "1-2048")
+# this functions populates `arr_words` with words
+gen_arr_words() {
+    arr_words=()
+    wordcount="$(ask_wordcount)"
 
-    sed -n "${number}"p "$wordlist"
-}
-
-gen_pass_words() {
-    array_words=()
-    wordlength=$(ask_number_words)
-    length=($(seq 1 "$wordlength"))
-
-    for i in "${length[@]}"; do
-        word=$(get_word)
-
-        # separate array items
-        #test_array+=("${word@u}")
-
-        # one single array item
-        array_words+="${word@u}"
+    for ((i = 0; i < "$wordcount"; i++)); do
+        rand=$(get_rng "1-2048")
+        word=$(sed -n "${rand}"p "$wordlist")
+        arr_words+="${word@u}"
     done
-
-    #echo "${array_words[@]}"
 }
 
 #####################################################################
 
-ask_number_numbers() {
-    read -r -p "How many numbers you want? " number_numbers
+ask_numcount() {
+    read -r -p "How many numbers do you want in the password? " numcount
 
-    while ! echo "$number_numbers" | grep -q "^[0-9][0-9]*$"; do
-        read -r -p "A NUMBER, PLEASE." number_numbers
+    while ! echo "$numcount" | grep -q "^[0-9][0-9]*$"; do
+        read -r -p "A NUMBER, PLEASE." numcount
     done
 
-    echo "$number_numbers"
+    echo "$numcount"
 }
 
-gen_pass_numbers() {
-    array_numbers=()
-    numlength=$(ask_number_numbers)
-    length=($(seq 1 "$numlength"))
+# this function populates `array_nums` with numbers
+gen_arr_nums() {
+    arr_nums=()
+    numlength=$(ask_numcount)
 
-    for i in "${length[@]}"; do
-        number=$(get_rng "0-9")
-
-        # separate array items
-        #array_numbers+=("${number}")
-
-        # one single array item
-        array_numbers+="${number}"
+    for ((i = 0; i < "$numlength"; i++)); do
+        num=$(get_rng "0-9")
+        arr_nums+="${num}"
     done
-
-    #echo "${array_numbers[@]}"
 }
 
 #####################################################################
 
 main() {
     get_wordlist
-
-    gen_pass_words
-
-    gen_pass_numbers
-
-    echo "${array_words}${array_numbers}"
+    gen_arr_words
+    gen_arr_nums
+    echo "${arr_words}${arr_nums}"
 }
 
 main
